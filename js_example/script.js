55c36f26e0f6d8cf3bd2ffecbc702077d8c6eba0
Luis <lfr4704@gmail.com>
2018-02-21 10:32:27 -0800

    revised

---
+++ js_example/script.js
@@ -0,0 +1,38 @@
+var name = prompt("Input your name:"); //this is varialble that will save the input into variable name
+
+
+/* this is for 
+multi-line project
+*/
+
+console.log(name);
+
+var x = 100;
+
+var y = 2;
+
+var z = x + y;
+
+console.log(z);
+
+console.log(true,false);
+
+if ( false ) {
+	console.log("hello");
+} else {
+	console.log("it is false");
+}
+
+var count = 0;
+var firstLine = "*"
+while ( count < 20) {
+	
+	firstLine = firstLine + "*";
+
+	if (count % 2 === 1) {
+		firstLine = firstLine + " "
+	}
+
+	count++
+	console.log(firstLine);
+}
