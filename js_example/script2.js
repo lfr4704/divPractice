833f9e855da4382d8e47c87890dbbf61ef7148b5
Luis <lfr4704@gmail.com>
2018-02-21 12:39:24 -0800

    revised

---
+++ js_example/script2.js
@@ -0,0 +1,27 @@
+
+var number1 = 0;
+var number2 = 10;
+var letter1 = "a";
+var letter2 = "b";
+var compOperator = true;
+
+/*var num1=20, text3= "text", sunny = "true";
+
+while (number1 <=20) {
+	console.log(number1);
+	number1++
+}
+
+for (i = 0; i < 20; i++) {
+	console.log("goodbye");
+
+	for (e = 0; e < 10; e++) {
+		console.log("inner loop");
+	}
+}*/
+
+var myfirstArray = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+
+myfirstArray[3] = "does not exist"; //this is how you change existing data from array.
+
+var mycurrentday = myfirstArray[3];  //this is how you pull data from array.
